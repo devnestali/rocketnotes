bb4d561a310013ec61d283394afd7527fa8ec4d2
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-26 16:06:21 +0200

    Installing and testing styled-components

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello Word! My name is Victor </h1>
+      <span>Victor Nestali</span>
+    </Container>
+  )
+}
