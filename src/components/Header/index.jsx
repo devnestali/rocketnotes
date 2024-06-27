58b64025808a29e88550f40e643eaefde585dce3
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-27 13:59:29 +0200

    Putting user image and apresentation in Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
   return (
     <Container>
       
+      <Profile>
+        <img 
+          src="https://github.com/devnestali.png" 
+          alt="Foto do usuário"  
+        />
+
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Victor Nestali</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
