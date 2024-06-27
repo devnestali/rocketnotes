13b092c2aee43a8e177582d831d1c29551175477
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-27 11:02:14 +0200

    Customizing fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -10,7 +10,15 @@ export default createGlobalStyle`
   body {
     background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
     color: ${({ theme }) => theme.COLORS.WHITE };
+    
+    -webkit-font-smoothing: antialiased;
   }
+
+  border-style, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+    outline: none;
+  } 
 
   a {
     text-decoration: none;
