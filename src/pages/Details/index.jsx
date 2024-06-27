91690f3c6f0b2ec053874136466e1981d22f63fb
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-27 11:21:31 +0200

    Creating components and button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Details() {
   return (
     <Container>
       <h1>Hello Word! My name is Victor </h1>
       <span>Victor Nestali</span>
+
+      <Button />
     </Container>
   )
 }
